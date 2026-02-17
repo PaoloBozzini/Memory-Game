e08c705b5c312647d607aefb37794dfdd9a3248a
Ahmadreza Pakraei <arpakraei@gmail.com>
2026-02-17 15:56:24 +0100

    fix: configure CORS in server to allow frontend API requests

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
+import cors from "cors";
 const port=3000;
 const app=express();
 
 import cardsRoutes from "./routes/cards/card.read.js";
+app.use(cors());
 app.use(express.json());
 app.use('/cards',cardsRoutes);
 
 
 app.listen(port , () =>{
 
-    console.log(`Server runs on http://localhost:${port}`);}
+    console.log(`Server run on http://localhost:${port}`);}
 
 );
