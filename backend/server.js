f2b1e5d69cfee0330ba2c6df465db1652c32c710
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-02-22 00:10:16 +0100

    update package.json for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,34 @@
 import express from "express";
 import cors from "cors";
-const port=3000;
-const app=express();
-
 import cardsRoutes from "./routes/cards/card.read.js";
+
+//
+import path from "path";
+import { fileURLToPath } from "url";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use('/cards',cardsRoutes);
+
+//API routes
+app.use("/cards", cardsRoutes);
+
+// Fix for ES modules: __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
-app.listen(port , () =>{
+// Serve all files in frontend folder (CSS, JS, images)
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-    console.log(`Server run on http://localhost:${port}`);}
+// Serve index.html on "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
-);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
