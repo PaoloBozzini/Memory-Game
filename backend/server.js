c01f513f78981ccae521291a649d6b2af2b8e7a7
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-02-24 16:18:53 +0100

    fix path in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,14 +23,14 @@ const __dirname = path.dirname(__filename);
 
 
 // Serve all files in frontend folder (CSS, JS, images)
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, '..' ,"frontend")));
 
 
 
 
 // Serve index.html on "/"
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/index.html"));
+  res.sendFile(path.join(__dirname, '..' ,"frontend, 'index.html'"));
 });
 
 
